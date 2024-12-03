831807ea621310b97be0513031a44b9f27377b25
ironlanderl <fabrizio03p@gmail.com>
2024-12-03 10:53:21 +0100

    Aggiungi implementazione per il giorno 3 (parte 2 non funzionante) e aggiorna main per includere le nuove funzioni

---
+++ src/day3.rs
@@ -0,0 +1,76 @@
+use crate::utils::read_file;
+use regex::Regex;
+
+fn calculate_mults(mults: Vec<&str>) -> i64 {
+    // Extract the numbers in '1' and '2'
+    let mults_reg_val = Regex::new(r"(?<val1>\d+),(?<val2>\d+)").unwrap();
+    let mut total: i64 = 0;
+
+    for mult in mults {
+        let values: Vec<(i64, i64)> = mults_reg_val
+            .captures_iter(mult)
+            .map(|caps| {
+                // The unwraps are okay because every capture group must match if the whole
+                // regex matches, and in this context, we know we have a match.
+                //
+                // Note that we use `caps.name("y").unwrap().as_str()` instead of
+                // `&caps["y"]` because the lifetime of the former is the same as the
+                // lifetime of `hay` above, but the lifetime of the latter is tied to the
+                // lifetime of `caps` due to how the `Index` trait is defined.
+                let num1 = caps
+                    .name("val1")
+                    .unwrap()
+                    .as_str()
+                    .parse::<i64>()
+                    .ok()
+                    .unwrap();
+                let num2 = caps
+                    .name("val2")
+                    .unwrap()
+                    .as_str()
+                    .parse::<i64>()
+                    .ok()
+                    .unwrap();
+                (num1, num2)
+            })
+            .collect();
+
+        //println!("Huh? {:?}", values);
+
+        total += values[0].0 * values[0].1
+    }
+    total
+}
+
+pub fn day3a() {
+    let content = read_file("inputs/day3.txt".to_string());
+
+    // Extract conly mul(number,number)
+    let mults_reg = Regex::new(r"(?m)mul\(\d+,\d+\)").unwrap();
+
+    let mults: Vec<&str> = mults_reg.find_iter(&content).map(|m| m.as_str()).collect();
+    //println!("{:?}", mults);
+
+    let total: i64 = calculate_mults(mults);
+
+    println!("Total: {:?}", total);
+}
+
+pub fn day3b() {
+    let mut content = read_file("inputs/day3.txt".to_string());
+
+    // Welp. It's fucking absurd regex time.
+    // Remove everything between don't and do.
+    let cleanup_reg = Regex::new(r"don't\(\)\S*?do\(\)").unwrap();
+    content = cleanup_reg.replace_all(&content, "").to_string();
+
+    // Extract conly mul(number,number)
+    let mults_reg = Regex::new(r"(?m)mul\(\d+,\d+\)").unwrap();
+
+    let mults: Vec<&str> = mults_reg.find_iter(&content).map(|m| m.as_str()).collect();
+    //println!("{:?}", mults);
+
+    let total: i64 = calculate_mults(mults);
+
+    println!("Total: {:?}", total);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 mod day1;
 mod day2;
+mod day3;
 
 mod utils;
 
 use day1::{day1a, day1b};
 use day2::{day2a, day2b};
+use day3::{day3a, day3b};
 
 fn main() {
     println!("Day 1");
     //day1a();
     //day1b();
     println!("Day 2");
-    day2a();
-    day2b();
+    //day2a();
+    //day2b();
+    println!("Day 3");
+    day3a();
+    day3b();
 }
